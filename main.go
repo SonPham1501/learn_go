8b3c44317ad8b4a2e4ed98c64427dad4423fe576
SON <son@SONs-Mac-mini.local>
2022-11-09 15:51:35 +0700

    pipeline

--- main.go
+++ main.go
@@ -2,13 +2,64 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"sync"
 )
 
+var wg = sync.WaitGroup{}
+
 func main() {
-	i := "12"
+	rundomNumbers := []int{}
+	for i := 1; i <= 1000; i++ {
+		rundomNumbers = append(rundomNumbers, i)
+	}
 
-	j, er := strconv.Atoi(i)
+	inputChan := generatePipeline(rundomNumbers)
 
-	fmt.Printf("%T, %v", er, j)
+	//fan-out
+	c1 := fanOut(inputChan)
+	c2 := fanOut(inputChan)
+	c3 := fanOut(inputChan)
+
+	//fan-in
+	c := fanIn(c1, c2, c3)
+	sum := 0
+	for i := 0; i < len(rundomNumbers); i++ {
+		sum += <-c
+	}
+	fmt.Println(sum)
+}
+
+func generatePipeline(numbers []int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range numbers {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func fanOut(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func fanIn(inputChannel ...<-chan int) <-chan int {
+	in := make(chan int)
+	go func() {
+		for _, c := range inputChannel {
+			for n := range c {
+				in <- n
+			}
+		}
+		close(in)
+	}()
+	return in
 }
