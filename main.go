84dbc742d26f351d0ab05de7a4e331f2d317cacf
SON <son@SONs-Mac-mini.local>
2022-12-22 16:24:12 +0700

    use gin

--- main.go
+++ main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	router := gin.Default()
+
+	api := router.Group("/api")
+	{
+		api.GET("/get/:id", func(c *gin.Context) {
+			id := c.Param("id")
+			c.JSON(http.StatusOK, gin.H{
+				"message": "ping pong successful: " + id,
+			})
+		})
+
+		api.POST("/create", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H {
+				"message": "Create new post sucessful",
+			})
+		})
+	}
+
+	router.Run(":8080")
 }
